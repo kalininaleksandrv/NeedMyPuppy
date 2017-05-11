1d1f58bcf896361fb13dc2d9846289da1d551d73
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2017-05-11 21:04:13 +0300

    add fragment with buttons 2 activity

--- app/src/main/java/dev/eyesless/needmypuppy/DescriptionActivity.java
+++ app/src/main/java/dev/eyesless/needmypuppy/DescriptionActivity.java
@@ -3,6 +3,8 @@ package dev.eyesless.needmypuppy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.function.ToDoubleBiFunction;
+
 public class DescriptionActivity extends AppCompatActivity {
 
     public static final String GETBREEDID = "getbreedid";
@@ -12,12 +14,17 @@ public class DescriptionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
 
+        //вызываем фрагмент breed description
+
         Fragment_description mydescription = (Fragment_description)getFragmentManager()
                 .findFragmentById(R.id.frame_breed_description);
 
         //вызываем метод fragment_description чтобы передать ID отображаемого айтема
 
         mydescription.setBreedId((int)getIntent().getExtras().get("getbreedid"));
+
+        //вызываем фрагмент btns
+        // TODO: 11.05.2017 здесь вызываем метод фрагмента nxtbtn для передачи ему (как?) количества айтемов в списке
 
 
     }
