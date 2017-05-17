c4b34f0d9cb4a6bec297ec2db8e3e878090cf6e1
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2017-05-17 12:58:45 +0300

    working version

--- app/src/main/java/dev/eyesless/needmypuppy/DescriptionActivity.java
+++ app/src/main/java/dev/eyesless/needmypuppy/DescriptionActivity.java
@@ -6,21 +6,12 @@ import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-
 
 public class DescriptionActivity extends AppCompatActivity implements Fragment_nxtbtn.onButtonListner
  {
-
     public static final String GETBREEDID = "getbreedid";
-
-
-
-     private int interID;
+    private int interID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +20,6 @@ public class DescriptionActivity extends AppCompatActivity implements Fragment_n
 
         //если активность создается впервые получаем ссылку на фрагмент breed description через создание его экземпляра
         // если повторно - просто берем значение ID из сейва
-
         if (savedInstanceState != null){
 
             int i = savedInstanceState.getInt("interID");
@@ -37,60 +27,55 @@ public class DescriptionActivity extends AppCompatActivity implements Fragment_n
 
         } else {
 
-        setInterID((int)getIntent().getExtras().get("getbreedid"));
-        Fragment_description newdescription = new Fragment_description();
-        newdescription.setBreedId(getInterID());
-        FragmentTransaction fratra = getFragmentManager().beginTransaction();
-        fratra.replace(R.id.frame_breed_description, newdescription);
-        fratra.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        fratra.commit();}
+            setInterID((int)getIntent().getExtras().get("getbreedid"));
+
+            //запускаем отдельный метод замещающий фрагменты
+            fragmentReplacer();
+        }
 
     }
 
     @Override
     public void buttonClicked(View v) {
 
+        //создаем экземпляры кнопок для установки в дальнейшем состояния и цветового мода
         ImageButton prev = (ImageButton) v.findViewById(R.id.nxtbtn_imageButton_prev);
         ImageButton next = (ImageButton) v.findViewById(R.id.nxtbtn_imageButton_next);
 
-
+        // переключатель получает вью кнопки и в зависимости от id уменьшает или увеличивает счетчик
+        // после чего запускает заместитель фрагментов
             switch (v.getId()) {
                   case R.id.nxtbtn_imageButton_next:
 
                       setInterID(getInterID()+1);
 
                       if (fragmentCounter(getInterID())){
-
-                          fragmentOnClickReplacer();
-
+                          fragmentReplacer();
                          }
-
                       else {
                           next.setEnabled(false); //если дошел до последнего айтема установим кнопке falce (см onClickReplacer)
                           next.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
                           setInterID(getInterID()-1);
                       }
-
-
                   break;
+
                   case R.id.nxtbtn_imageButton_prev:
 
                       setInterID(getInterID()-1);
                       if (fragmentCounter(getInterID())){
-                          fragmentOnClickReplacer();
+                          fragmentReplacer();
                       }
                       else {
                           prev.setEnabled(false);
                           prev.setColorFilter(Color.GRAY, PorterDuff.Mode.SRC_IN);
                           setInterID(getInterID()+1);
                       }
-
                   break;
             }
-
     }
 
-    public void fragmentOnClickReplacer (){
+    //заместитель фрагментов
+    public void fragmentReplacer (){
 
         Fragment_nxtbtn mynxtbtn = (Fragment_nxtbtn) getSupportFragmentManager().findFragmentById(R.id.frame_nxtbtn);
         Fragment_description newdescription = new Fragment_description();
@@ -112,25 +97,19 @@ public class DescriptionActivity extends AppCompatActivity implements Fragment_n
     }
 
     //метод для установки значения при переключении фрагмента, получает на вход лист коллекции чтобы узнать обще кол-во записей и теекущую запись
-
     public boolean fragmentCounter (int count){
 
-
         if (count > Main_logic.finalListOfBreedTitles.size()-1 || count<0){
-
 
            return false;
 
         } else return true;
-
     }
 
     //геттер и сеттер для interID
-
      public int getInterID() {
          return interID;
      }
-
      public void setInterID(int interID) {
          this.interID = interID;
      }
