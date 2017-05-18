47d24ff1788e13c903d19143b7561391505f9ace
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2017-05-18 18:10:27 +0300

    add action menu

--- app/src/main/java/dev/eyesless/needmypuppy/MainActivity.java
+++ app/src/main/java/dev/eyesless/needmypuppy/MainActivity.java
@@ -3,14 +3,11 @@ package dev.eyesless.needmypuppy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -91,6 +88,32 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_mainactivity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.action_saveoptions:
+                //do something
+                return true;
+            case R.id.action_email:
+                //do something
+                return true;
+            case R.id.action_settings:
+                //do something
+                return true;
+
+          default: return super.onOptionsItemSelected(item);
+        }
+
+
+    }
 }
 
     /*
