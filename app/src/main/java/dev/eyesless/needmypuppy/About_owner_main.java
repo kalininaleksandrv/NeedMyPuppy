6aaa4583eaab74dbf814d2e5c0f8263b66968d6a
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2017-05-24 10:07:29 +0300

    add frame about_owner

---
+++ app/src/main/java/dev/eyesless/needmypuppy/About_owner_main.java
@@ -0,0 +1,29 @@
+package dev.eyesless.needmypuppy;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class About_owner_main extends Fragment {
+
+
+    public About_owner_main() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.about_owner_main, container, false);
+    }
+
+}
--- app/src/main/java/dev/eyesless/needmypuppy/Frame_main.java
+++ app/src/main/java/dev/eyesless/needmypuppy/Frame_main.java
@@ -25,12 +25,20 @@ public class Frame_main extends Fragment {
                              Bundle savedInstanceState) {
 
 
-        Buttons_main newButtonsMain = new Buttons_main();
+//        Buttons_main newButtonsMain = new Buttons_main();
+//        FragmentTransaction fratramain = getChildFragmentManager().beginTransaction();
+//        fratramain.replace(R.id.replaced_main, newButtonsMain);
+//        fratramain.addToBackStack(null);
+//        fratramain.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+//        fratramain.commit();
+
+        About_owner_main newAboutOwner = new About_owner_main();
         FragmentTransaction fratramain = getChildFragmentManager().beginTransaction();
-        fratramain.replace(R.id.buttons_main, newButtonsMain);
+        fratramain.replace(R.id.replaced_main, newAboutOwner);
         fratramain.addToBackStack(null);
         fratramain.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
         fratramain.commit();
+
 
 
         return inflater.inflate(R.layout.fragment_frame_main, container, false);
