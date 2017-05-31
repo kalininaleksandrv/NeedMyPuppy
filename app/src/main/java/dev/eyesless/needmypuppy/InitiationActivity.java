015cedb3af3492a9d1ca713fdaf894e6cc530ffd
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2017-05-31 14:16:24 +0300

    add class bucket and 5 buckets to init class, custom list view and custom list adapter

--- app/src/main/java/dev/eyesless/needmypuppy/InitiationActivity.java
+++ app/src/main/java/dev/eyesless/needmypuppy/InitiationActivity.java
@@ -1,21 +1,17 @@
 package dev.eyesless.needmypuppy;
 
-import android.app.Activity;
 import android.app.Application;
-import android.os.Bundle;
-import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class InitiationActivity extends Application {
-
-
 
     private String [] spinner_exp_array = {"есть ли опыт содержания и воспитания собак?",
             "нет опыта", "опыт минимальный", "я довольно опытный", "эксперт"};
 
     private String [] spinner_time_array = {"cколько времени готов тратить на собаку?",
             "не более 1 часа в день", "не более 2-3 часов в день", "все свободное время"};
-
-
 
     private String [] drawer_titles = {"сохранить ответы",
             "загрузить ответы", "написать разработчику"};
@@ -29,4 +25,20 @@ public class InitiationActivity extends Application {
     }
 
     public String[] getDrawer_titles() { return drawer_titles; }
+
+    MyBucket obidience = new MyBucket(getString(R.string.obidience), 1);
+    MyBucket guard = new MyBucket(getString(R.string.guard), 1);
+
+// TODO: 31.05.2017 make agressive, active, size
+
+    ArrayList<MyBucket> mybuckelisttmaker () {
+
+        ArrayList<MyBucket> mybusketslist = new ArrayList<>();
+
+        mybusketslist.add(obidience);
+        mybusketslist.add(guard);
+
+        return mybusketslist;
+    }
+
 }
---
+++ app/src/main/java/dev/eyesless/needmypuppy/List_profile.java
@@ -0,0 +1,13 @@
+package dev.eyesless.needmypuppy;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class List_profile extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
